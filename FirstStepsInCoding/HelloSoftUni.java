4c36a4176b5ff7936997138c6f920557e7a6cdf0
Petyo Veselinov <borovaneca@virgilio.it>
2024-02-01 22:01:04 +0200

    Update HelloSoftUni.java

--- FirstStepsInCoding/HelloSoftUni.java
+++ FirstStepsInCoding/HelloSoftUni.java
@@ -3,6 +3,6 @@ package FirstStepsInCoding;
 public class HelloSoftUni {
     public static void main(String [] args){
 
-        System.out.println("Zdarvei mamo obicham te!");
+        System.out.println("Hello SoftUni");
     }
 }
